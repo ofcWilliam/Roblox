cddefb8db939b8fafbfd5f0106a8ab096d47da76
ofcWilliam <william@rogerlindgren.se>
2025-01-01 14:06:41 +0100

    Upload

---
+++ Scripts/Infinite Yield.lua
@@ -0,0 +1,2 @@
+-- Load Infinite Yield (https://github.com/EdgeIY)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))()
---
+++ Scripts/SystemBroken.lua
@@ -0,0 +1,2 @@
+-- Load SystemBroken (https://github.com/H20CalibreYT)
+loadstring(game:HttpGet('https://raw.githubusercontent.com/H20CalibreYT/SystemBroken/main/script'))()
